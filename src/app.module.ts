cd10adae1a32a0b056aae3e7546bba8ae3326ccc
Phurich Chanchaichoojit <6810110268@psu.ac.th>
2025-12-17 19:23:46 +0700

    CheckPoint 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookCategoryModule } from './book-category/book-category.module';
+import { BookModule } from './book/book.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { BookCategoryModule } from './book-category/book-category.module';
       synchronize: true, // สร้าง Table อัตโนมัติ (ใช้สำหรับ Dev เท่านั้น)
     }),
     BookCategoryModule,
+    BookModule,
   ],
 })
 export class AppModule {}
--- src/book-category/dto/create-book-category.dto.ts
+++ src/book-category/dto/create-book-category.dto.ts
@@ -1,1 +1,12 @@
-export class CreateBookCategoryDto {}
+
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateBookCategoryDto {
+  @IsString()        // ต้องเป็นตัวหนังสือ
+  @IsNotEmpty()      // ห้ามว่างเปล่า
+  name: string;
+
+  @IsString()        // ต้องเป็นตัวหนังสือ
+  @IsOptional()      // จะมีหรือไม่ก็ได้ (ไม่บังคับ)
+  description?: string;
+}
